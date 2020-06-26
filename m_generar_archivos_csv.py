72bdca95bfd654b50a12bee1228495285ec4118b
Alfonso Campodonico <alfonso.campodonico@gmail.com>
2020-06-26 16:01:59 -0300

    Probando

---
+++ m_generar_archivos_csv.py
@@ -0,0 +1,13 @@
+##Test inicial
+def leer_txt():
+    with open("programas.txt") as f:
+        lines = f.read().splitlines()
+        return lines
+
+def programas_a_analizar(lst):
+    for i in lst:
+        with open(i) as f:
+            lines = f.read().splitlines()
+            print(lines)
+                  
+programas_a_analizar(leer_txt())
