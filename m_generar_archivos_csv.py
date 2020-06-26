5c0fbaaa5f8202af956c11bb946817922ad3978f
Alfonso Campodonico <alfonso.campodonico@gmail.com>
2020-06-26 16:53:01 -0300

    Imprime linea funciones

--- m_generar_archivos_csv.py
+++ m_generar_archivos_csv.py
@@ -1,13 +1,17 @@
-##Test inicial
 def leer_txt():
-    with open("programas.txt") as f:
-        lines = f.read().splitlines()
-        return lines
+    with open('programas.txt') as f:
+        modulos = [linea.rstrip('\n') for linea in f]
+        return modulos
+        
+def leer_py(mod):
+    for modulos in mod:
+        with open(modulos) as f:
+            modulos = [linea.rstrip('\n') for linea in f]
+            for lineas in modulos:
+                if lineas.startswith('def'):
+                    print(lineas)
+                   
+        print(modulos)
+                                 
+leer_py(leer_txt())
 
-def programas_a_analizar(lst):
-    for i in lst:
-        with open(i) as f:
-            lines = f.read().splitlines()
-            print(lines)
-                  
-programas_a_analizar(leer_txt())
