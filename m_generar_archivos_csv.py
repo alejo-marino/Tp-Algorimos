6e8f998674f3a6c06284a9238d276575fe1225c4
Luis <luisandrade195@gmail.com>
2020-06-26 17:30:20 -0300

    probando, solo ajuste espacios nada mas

--- m_generar_archivos_csv.py
+++ m_generar_archivos_csv.py
@@ -1,16 +1,19 @@
 txt = 'programas.txt'
 
+
 def leer(archivo):
     with open(archivo) as f:
         lineas = [linea.rstrip('\n') for linea in f]
         return lineas
-        
+
+
 def leer_py(archivo):
-        modulos = leer(archivo)
-        for modulo in modulos:
-            lineas = leer(modulo)
-            for linea in lineas:
-                if linea.startswith('def '):
-                    print(linea)
-                                   
+    modulos = leer(archivo)
+    for modulo in modulos:
+        lineas = leer(modulo)
+        for linea in lineas:
+            if linea.startswith('def '):
+                print(linea)
+
+
 leer_py(txt)
