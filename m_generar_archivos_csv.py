3441725358a37261b0af2eaf36196ae1093e29d8
Dan-Desktop <dan.bero15@gmail.com>
2020-06-28 20:31:54 -0300

    ordena los datos y despues los guarda en el csv

--- m_generar_archivos_csv.py
+++ m_generar_archivos_csv.py
@@ -4,6 +4,10 @@ txt = 'programas.txt'
 
 def leer(archivo):
     
+    """ El comenterario tiene que ser excluido """
+    
+    # El comenterario tiene que ser excluido
+
     lineas = [linea.rstrip('\n') for linea in archivo]
     
     return lineas
@@ -18,6 +22,7 @@ def abro_ar(archivo):
 
 def leer_py(archivo):
     import modulo_csv
+    datos = {}
     modulos = abro_ar(archivo)
     for modulo in modulos:
         lineas = abro_ar(modulo)
@@ -28,21 +33,23 @@ def leer_py(archivo):
                 index_inicial = lineas.index(funcion) +1
                 nombre_funcion = funcion.split('def ')[1].lstrip().split('(')[0]
                 parametros = funcion.split('(')[1].lstrip().split(')')[0]
-                     
+
             elif linea.strip().startswith('return'):
                 linea_return = linea
                 index_final = lineas.index(linea_return) + 1
                 cuerpo = lineas[index_inicial:index_final]
-                datos = nombre_funcion,parametros,modulo,cuerpo
-                modulo_csv.armo_csv(datos)
+                
+                datos[nombre_funcion] = [parametros,modulo,cuerpo]
+           
+    funciones_alfabeto = sorted(datos.items(),key = lambda clave: clave[0], reverse = True)
 
-    return None
-
+    return modulo_csv.armo_csv(funciones_alfabeto)
 
 
 def main():
 
     leer_py(txt)
+    
 
     return None
 
