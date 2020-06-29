318f2abb89cf11c6b20b2725cc2cb397b88ae44a
Dan-Desktop <dan.bero15@gmail.com>
2020-06-29 17:46:20 -0300

    cambio en el modulo_csv write en una linea

--- modulo_csv.py
+++ modulo_csv.py
@@ -8,14 +8,13 @@ def armo_csv(dic_ordenado):
             parametros = clave[1][0]
             modulo = clave[1][1]
             cuerpo = clave[1][2]
-
-            codigo.write("Nombre de la funcion \t\t Parametros formales \t\t Nombre del modulo")
-            codigo.write("\n\t\t"+nombre_funcion)
-            codigo.write(",\t\t\t\t\t\t"+parametros)
-            codigo.write(",\t\t\t\t"+modulo+",\n\n")
-            codigo.write("Cuerpo de la funcion:\n\n")
+            
             funcion = "\n".join(cuerpo)
-            codigo.write(funcion+"\n\n")
+            
+            codigo.write("Nombre de la funcion \t\t Parametros formales \t\t Nombre del modulo"+"\n\t\t"+nombre_funcion+",\t\t\t\t\t\t"+parametros+",\t\t\t\t"+modulo+",\n\n"+"Cuerpo de la funcion:\n\n"+funcion+"\n\n")
+            
+            """codigo.write(nombre_funcion+","+parametros+","+modulo+","+funcion+) ESTE LO CARGA MAS LIMPIO"""
+            
             """codigo.write(nombre_funcion+","+parametros+","+modulo+","+"\n\n"+funcion+"\n\n") ESTE LO CARGA MAS LIMPIO"""
     
     return None
