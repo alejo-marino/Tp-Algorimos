ee68f789212f35b23c1bba189fa3316038ff45ed
Dan-Desktop <dan.bero15@gmail.com>
2020-06-29 18:51:52 -0300

    cambios en modulo csv

--- modulo_csv.py
+++ modulo_csv.py
@@ -2,6 +2,8 @@ def armo_csv(dic_ordenado):
     
     with open ("fuente_unico.csv","a") as codigo:
         
+        codigo.write("Nombre de la funcion \t\t Parametros formales \t\t Nombre del modulo\n\n")
+       
         for clave in dic_ordenado:
             
             nombre_funcion = clave[0]
@@ -11,7 +13,7 @@ def armo_csv(dic_ordenado):
 
             funcion = "\n".join(cuerpo)
             
-            codigo.write("Nombre de la funcion \t\t Parametros formales \t\t Nombre del modulo"+"\n\t\t"+nombre_funcion+",\t\t\t\t\t\t"+parametros+",\t\t\t\t"+modulo+",\n\n"+"Cuerpo de la funcion:\n\n"+funcion+"\n\n")
+            codigo.write(nombre_funcion+",\t\t\t\t\t\t"+parametros+",\t\t\t\t"+modulo+",\n\n"+"Cuerpo de la funcion:\n\n"+funcion+"\n\n")
             
             """codigo.write(nombre_funcion+","+parametros+","+modulo+","+funcion) ESTE LO CARGA MAS LIMPIO"""
            
